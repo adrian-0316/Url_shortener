3a2dafb9a63ecdb5f9073035972ddc4cf3ddaa25
Adrian <112898743+adrian-0316@users.noreply.github.com>
2025-12-11 22:44:00 +0300

    util class added

---
+++ src/main/java/com/example/URL/Shortener/util/Base62.java
@@ -0,0 +1,18 @@
+package com.example.URL.Shortener.util;
+
+
+public final class Base62 {
+    private static final String ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final int BASE = ALPHABET.length();
+
+
+    public static String encode(long num) {
+        if (num == 0) return "0";
+        StringBuilder sb = new StringBuilder();
+        while (num > 0) {
+            sb.append(ALPHABET.charAt((int)(num % BASE)));
+            num /= BASE;
+        }
+        return sb.reverse().toString();
+    }
+}
